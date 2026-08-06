f2ebd7e6627e89b2bb53b742e90717e298f59849
alexmuinde <alexmuinde470@gmail.com>
2026-08-06 22:02:56 +0300

    create pages and routes

---
+++ client/pages/TruckMovementDocument.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TruckMovementDocument() {
+  return (
+    <div>TruckMovementDocument</div>
+  )
+}
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import {BrowserRouter , Routes, Route} from 'react-router-dom'
+import SignIn from '/pages/SignIn'
+import SignUp from '/pages/SignUp'
+import Home from '/pages/Home'
+import Profile from '/pages/Profile'
+import TruckMovementDocument from '/pages/TruckMovementDocument'
 
 export default function App() {
-  return (
-    <h1 className='text-red-500'>Kino
-    </h1>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signIn" element={<SignIn />} />
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/truckMovementDocument" element={<TruckMovementDocument />} />
+    </Routes>
+  </BrowserRouter>
 }
