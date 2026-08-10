35c502e9e91c2987ed96d6729b885c13864b3461
alexmuinde <alexmuinde470@gmail.com>
2026-08-10 21:03:57 +0300

    complete login functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,12 +1,12 @@
 import User from '../models/userModel.js'
-import bcrypt from 'bcryptjs'
+import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js'
 import jwt from 'jsonwebtoken'
 
 
 export const signUp = async (req, res, next) => {
  const { username, email, password } = req.body
- const hashedPassword = await bcrypt.hashSync(password, 10)
+ const hashedPassword =  bcryptjs.hashSync(password, 10)
  const newUser = new User({ username, email, password: hashedPassword })
     try {
         await newUser.save()
@@ -19,17 +19,18 @@ export const signUp = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
     const { email, password } = req.body
+    
     try {
         const validUser = await User.findOne({ email })
         if (!validUser) {
             return next(errorHandler(404, 'User not found'))
         }
-        const validPassword = await bcryptjs.compareSync(password, validUser.password)
+        const validPassword =  bcryptjs.compareSync(password, validUser.password)
         if (!validPassword) {
             return next(errorHandler(401, 'Invalid credentials'))
         }
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
-        const { password : pass, ...rest } = validUser._doc
+        const { password: pass, ...rest } = validUser._doc
         res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
     } catch (error) {
         next(error)
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
+  const[formData, setFormData] = useState({})
+  const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+  
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.id]: e.target.value })
+    }
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault()
+      try {
+        setLoading(true)
+        const res = await fetch('api/auth/login',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+          }
+        )
+        const data = await res.json()
+        if(data.success === false) {
+          setLoading(false)
+          setError(data.message)
+          return
+      }
+      setLoading(false)
+      setError(null)
+      navigate('/')
+      } catch (error) {
+        setLoading(false)
+        setError('An error occurred while logging in.')
+      }
+      
+    }
+    console.log(formData)
   return (
     <div>
       <h1 className='text-3xl text-center font-semibold my-7'>Please Login</h1>
-      <form className='flex flex-col w-1/3 mx-auto'>
-        <input type="email" placeholder='Email' className='border-b-2 border-gray-300  p-2 mb-4 focus:outline-none focus:border-blue-500' />
-        <input type="password" placeholder='Password' className='border-b-2 border-gray-300  p-2 mb-4 focus:outline-none focus:border-blue-500' />
-        <button type="submit" className='bg-blue-500 text-white rounded-md p-2 hover:bg-blue-600'>Login</button>
+      <form onSubmit={handleSubmit} className='flex flex-col w-1/3 mx-auto'>
+        <input onChange={handleChange} id="email" required type="email" placeholder='Email' className='border-b-2 border-gray-300  p-2 mb-4 focus:outline-none focus:border-blue-500' />
+        <input onChange={handleChange} id="password" required type="password" placeholder='Password' className='border-b-2 border-gray-300  p-2 mb-4 focus:outline-none focus:border-blue-500' />
+        <button disabled={loading} type="submit" className='bg-blue-500 text-white rounded-md p-2 hover:bg-blue-600'>
+          {loading ? 'Loading...' : 'Login'}
+        </button>
       </form>
-      <p className='text-center mt-4'>Don't have an account? <a href="/signUp" className='text-blue-500 hover:underline'>Sign Up</a></p>
+      <p className='text-center mt-4'>Dont have an account? <a href="/signUp" className='text-blue-500 hover:underline'>Sign Up</a></p>
+      {error && <p className='text-red-500 text-center mt-4'>{error}</p>}
     </div>
   )
 }
