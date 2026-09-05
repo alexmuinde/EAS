05814cb21e32bda8b35ae09c2c6e0d346eef073c
alexmuinde <alexmuinde470@gmail.com>
2026-09-05 23:22:19 +0300

    misspelling kidogo

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { updateUser } from '../controllers/userControllers.js'
+import { updateUser } from '../controllers/userController.js'
 import verifyToken from '../utils/verifyUser.js'
 
 const router = express.Router()
